a5af5412ef07d08011570a7b44cc6ef09eebc51b
Siddhesh <siddhehsongire@gmail.com>
2023-06-30 15:21:53 +0530

    Making different pages and linking them in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
 function App() {
-  return <> Hi </>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
