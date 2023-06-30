95d5fcd62d3b0203bc1dd7721047bd0bb6452bf1
Siddhesh <siddhehsongire@gmail.com>
2023-06-30 17:21:44 +0530

    Register form connecting with backend

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <Toaster />
     </Router>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { server } from "../main";
+import toast from "react-hot-toast";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(name, email);
+
+    try {
+      const { data } = await axios.post(
+        `${server}/user/new`,
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      toast.success(data.message);
+    } catch (error) {
+      toast.error("Some Error");
+    }
+  };
+
+  return (
+    <div className="login">
+      <section>
+        <form onSubmit={submitHandler}>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Name"
+            required
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            required
+            placeholder="Password"
+          />
+          <button type="submit">Sign up</button>
+          <h4>Or</h4>
+          <Link to="/login">Log in</Link>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Register;
